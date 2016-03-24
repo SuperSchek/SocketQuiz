f7f8090f5ccf0f70b0ee5de4d2fae2e4a35978de
Sander Schekman <sanderschekman@gmail.com>
2016-03-24 13:27:47 +0100

    Added environment variable for port for heroku.

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ var express = require('express'),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
 
+/*
 server.listen(3000);
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+*/
+
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/index.html');
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
