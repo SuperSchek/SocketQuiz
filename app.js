681e706b4b0f9c930b9e9dc13caf98921335b0bb
SuperSchek <sanderschekman@gmail.com>
2016-03-24 17:24:34 +0100

    Refactored server code in app.js

--- app.js
+++ app.js
@@ -3,19 +3,11 @@ var express = require('express'),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
 
-/*
-server.listen(3000);
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-*/
-
 app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 }).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+    console.log('SocketQuiz draait nu op local op poort', app.get('port'));
 });
