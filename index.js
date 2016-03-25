b80351fe5bc656d99c8e59d6a632b70be9ab1b12
SuperSchek <sanderschekman@gmail.com>
2016-03-25 15:16:26 +0100

    Trying to get things up and running on Heroku. jQuery seems to be the problem.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 4000;
+var $ = require('jQuery');
+var port = process.env.PORT || 2000;
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
