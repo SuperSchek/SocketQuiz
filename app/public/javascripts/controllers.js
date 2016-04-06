8397a903fce80943a125da603db54823f10be296
Jacco Schumacher <jaccoschumacher@Jaccos-MacBook-Air.local>
2016-04-06 14:16:04 +0200

    geprobeerd routes aan te maken

--- app/public/javascripts/controllers.js
+++ app/public/javascripts/controllers.js
@@ -3,18 +3,19 @@
  */
 var app = angular.module('socket-quiz');
 
-app.controller('mainController', function($scope, socket) {
+app.controller('mainController', function($scope, $location, socket) {
     $scope.newCustomers = [];
     $scope.currentCustomer = {};
 
+    $scope.setRoute = function ( route) {
+        $location.path(route);
+    };
 
 });
-/*
+
 var lobby = angular.module('lobby');
 
-lobby.config(function ($routeProvider) {
+lobby.config('$routeProvider', function ($routeProvider) {
     $routeProvider
-        .when('/lobby', {templateUrl: 'partials/desktop/lobby.html'})
-        .otherwise({redirectTo: '/desktop'});
-});*/
-
+        .when('/desktop/lobby', {templateUrl: 'partials/desktop/lobby.html'})
+});
