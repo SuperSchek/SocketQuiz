4664f3fdce07d2c3a0744da3b6818c42645b72e3
Jacco Schumacher <jaccoschumacher@Jaccos-MacBook-Air.local>
2016-04-06 16:17:48 +0200

    doorklikken werkt

--- app/public/javascripts/controllers.js
+++ app/public/javascripts/controllers.js
@@ -6,16 +6,7 @@ var app = angular.module('socket-quiz');
 app.controller('mainController', function($scope, $location, socket) {
     $scope.newCustomers = [];
     $scope.currentCustomer = {};
-
-    $scope.setRoute = function ( route) {
+    $scope.setRoute = function(route) {
         $location.path(route);
     };
-
-});
-
-var lobby = angular.module('lobby');
-
-lobby.config('$routeProvider', function ($routeProvider) {
-    $routeProvider
-        .when('/desktop/lobby', {templateUrl: 'partials/desktop/lobby.html'})
 });
