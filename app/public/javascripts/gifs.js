1b7d9c17a84d6fb245a7e9b09d3c086b5bfe6887
Sander Schekman <sanderschekman@gmail.com>
2016-04-11 17:28:17 +0200

    Added Joffrey to the winner-gifs.

--- app/public/javascripts/gifs.js
+++ app/public/javascripts/gifs.js
@@ -1,14 +1,17 @@
-var winnerGifs = ["https://m.popkey.co/0b1935/qrOQG.gif", "https://m.popkey.co/6d9ceb/wrkk.gif"];
+var winnerGifs = ["https://m.popkey.co/0b1935/qrOQG.gif", "https://m.popkey.co/6d9ceb/wrkk.gif", "http://i.giphy.com/5BgqPLsvG8dMI.gif"];
 var loserGifs = ["https://m.popkey.co/0b1935/qrOQG.gif", "https://m.popkey.co/6d9ceb/wrkk.gif"];
 
 function printGifSucces() {
-    var randomnumber = Math.floor(Math.random() * 2);
+    var randomnumber = Math.floor(Math.random() * 3);
     if (randomnumber == 0) {
         angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[randomnumber] + ")no-repeat right");
         angular.element(document).find('#vraag-uitslag-mob').css("background-position", "0");
     } else if (randomnumber == 1) {
         angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[randomnumber] + ")no-repeat right");
         angular.element(document).find('#vraag-uitslag-mob').css("background-position-x", "-340px");
+    } else if (randomnumber == 2) {
+        angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[randomnumber] + ")no-repeat left");
+        angular.element(document).find('#vraag-uitslag-mob').css("background-position-x", "-160px");
     }
 }
 
