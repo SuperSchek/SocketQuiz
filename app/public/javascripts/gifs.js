2ef99a04575c0e0f87e3dee811bc76b368eee669
Sander Schekman <sanderschekman@gmail.com>
2016-04-12 11:07:18 +0200

    Fixed bug where correct/failure gifs weren't really random.

--- app/public/javascripts/gifs.js
+++ app/public/javascripts/gifs.js
@@ -2,36 +2,40 @@ var winnerGifs = ["../images/gifs/success/obama.gif", "../images/gifs/success/ki
                     "../images/gifs/success/bb-8.gif", "../images/gifs/success/joker.gif"];
 var loserGifs = ["../images/gifs/loser/ramsay.gif", "https://m.popkey.co/9305c4/bbGW.gif", "https://m.popkey.co/16ac19/eL4ZQ.gif"];
 
+var gifNr;
+
 function printGifSucces() {
-    var randomnumber = Math.floor(Math.random() * 5);
-    if (randomnumber == 0) {
-        angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[randomnumber] + ")no-repeat right");
+    gifNr = 0;
+    gifNr = Math.floor(Math.random() * 5);
+    if (gifNr == 0) {
+        angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[0] + ")no-repeat right");
         angular.element(document).find('#vraag-uitslag-mob').css("background-position", "0");
-    } else if (randomnumber == 1) {
-        angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[randomnumber] + ")no-repeat right");
+    } else if (gifNr == 1) {
+        angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[1] + ")no-repeat right");
         angular.element(document).find('#vraag-uitslag-mob').css("background-position-x", "-340px");
-    } else if (randomnumber == 2) {
-        angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[randomnumber] + ")no-repeat left");
+    } else if (gifNr == 2) {
+        angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[2] + ")no-repeat left");
         angular.element(document).find('#vraag-uitslag-mob').css("background-position-x", "-160px");
-    } else if (randomNumber == 3) {
-         angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[randomnumber] + ")no-repeat left");
+    } else if (gifNr == 3) {
+         angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[3] + ")no-repeat left");
          angular.element(document).find('#vraag-uitslag-mob').css("background-position-x", "-750px");
-    } else if (randomnumber == 4) {
-        angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[randomnumber] + ")no-repeat left");
+    } else if (gifNr == 4) {
+        angular.element(document).find('#vraag-uitslag-mob').css("background", "url(" + winnerGifs[4] + ")no-repeat left");
         angular.element(document).find('#vraag-uitslag-mob').css("background-position-x", "-490px");
     }
 }
 
 function printGifLoser() {
-    var randomnumber = Math.floor(Math.random() * 3);
-    if (randomnumber == 0) {
-        angular.element(document).find('#vraag-uitslag-fout-mob').css("background", "url(" + loserGifs[randomnumber] + ")no-repeat right");
+    gifNr = 0;
+    gifNr = Math.floor(Math.random() * 3);
+    if (gifNr == 0) {
+        angular.element(document).find('#vraag-uitslag-fout-mob').css("background", "url(" + loserGifs[0] + ")no-repeat right");
         angular.element(document).find('#vraag-uitslag-fout-mob').css("background-position-x", "-180px");
-    } else if (randomnumber == 1) {
-        angular.element(document).find('#vraag-uitslag-fout-mob').css("background", "url(" + loserGifs[randomnumber] + ")no-repeat right");
+    } else if (gifNr == 1) {
+        angular.element(document).find('#vraag-uitslag-fout-mob').css("background", "url(" + loserGifs[1] + ")no-repeat right");
         angular.element(document).find('#vraag-uitslag-fout-mob').css("background-position-x", "-75px");
-    } else if (randomnumber == 2) {
-        angular.element(document).find('#vraag-uitslag-fout-mob').css("background", "url(" + loserGifs[randomnumber] + ")no-repeat right");
+    } else if (gifNr == 2) {
+        angular.element(document).find('#vraag-uitslag-fout-mob').css("background", "url(" + loserGifs[2] + ")no-repeat right");
         angular.element(document).find('#vraag-uitslag-fout-mob').css("background-position-x", "-340px");
     }
 }
